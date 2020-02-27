3c48517fe33a226e86bd4706386560697890eadd
Wayne Pollock <pollock@acm.org>
2020-02-27 06:32:10 -0500

    Initial commit

---
+++ src/test/java/com/wpollock/MainTest.java
@@ -0,0 +1,14 @@
+package com.wpollock;
+
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+        System.out.println("Dummy test");
+        assertTrue( 1 == 1 );
+    }
+}
